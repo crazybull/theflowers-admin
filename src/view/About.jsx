575d6e094344d09a76e9e0247c1ddff95ea15252
crazybull <626066708@qq.com>
2021-02-17 16:20:54 +0800

    eject

---
+++ src/view/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class About extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={};
+  }
+  render(){
+    return (
+      <div>This is About</div>
+    )
+  }
+}
+
+export default About;
---
+++ src/view/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={};
+  }
+  render(){
+    return (
+      <div>This is Home</div>
+    )
+  }
+}
+
+export default Home;
